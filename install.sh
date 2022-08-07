3d2b83956ac680389f37b5f20073ff4cca27686b
Pedro Chagas <87217922+pChagas-cloud@users.noreply.github.com>
2022-08-06 22:14:19 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/bash
 
 mkdir bin
-mkdir $HOME/$(whoami)/.config/todo-python/tasklist
+mkdir $HOME/$(whoami)/.config/todo-python/
+touch $HOME/$(whoami)/.config/todo-python/tasklist
 cp main.py bin/kittykat
 cp bin/kittykat $HOME/.local/bin/
