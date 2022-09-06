d25fc585a8463dc7ff01eea96cee556b3b8d6714
Pedro Chagas <87217922+pChagas-cloud@users.noreply.github.com>
2022-09-06 11:53:12 -0300

    Delete install.sh

--- install.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/bash
-
-mkdir bin
-mkdir $HOME/$(whoami)/.config/todo-python/
-touch $HOME/$(whoami)/.config/todo-python/tasklist
-cp main.py bin/kittykat
-cp bin/kittykat $HOME/.local/bin/
